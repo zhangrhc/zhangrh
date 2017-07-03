9ad9d6472cb475abb468705f3ea13e0889057034
zhangrh <314714885@qq.com>
2017-07-03 17:35:52 +0800

    第一次

--- validate_button.py
+++ validate_button.py
@@ -38,11 +38,13 @@ class LoginCase(unittest.TestCase):
         self.assertTrue('机构版系统管理' in link)   #用assertTrue(x)方法来断言  bool(x) is True 登录成功后用户昵称在lnk_current_user里
         self.dr.get_screenshot_as_file("D:\\cnblogtest\\validate_button_success.jpg")  #截图  可自定义截图后的保存位置和图片命名
         sleep(1)
-        #选择平行的两组元素中第二个，所有的li标签id='pmng_admin'中第二个。机构版档案对照验证
+        #选择平行的两组元素中第二个，所有的li标签id='pmng_admin'中第二个。机构版档案对照、项目验证
         self.dr.find_element_by_xpath("//li[2][@id='pmng_admin']/a/span").click()
         sleep(1)
-        link1=self.dr.find_element_by_id('0001ZZ1000000001RYH5').text
-        self.assertTrue('机构档案对照' in link1)
+        link11=self.dr.find_element_by_id('0001ZZ1000000001RYH5').text
+        self.assertTrue('机构档案对照' in link11)
+        link12=self.dr.find_element_by_id('0001ZZ10000000022502').text
+        self.assertTrue('项目' in link12)
         #工作日志权限控制验证
         self.dr.find_element_by_xpath("//li[3][@id='pmng_admin']/a/span").click()
         sleep(1)
@@ -56,8 +58,8 @@ class LoginCase(unittest.TestCase):
         #自动验证规则设置
         self.dr.find_element_by_xpath("//li[5][@id='pmng_admin']/a/span").click()
         sleep(1)
-        link3=self.dr.find_element_by_id('0001ZZ1000000001SWFA').text
-        self.assertTrue('自动验证规则设' in link3)
+        link4=self.dr.find_element_by_id('0001ZZ1000000001SWFA').text
+        self.assertTrue('自动验证规则设' in link4)
 
     def tearDown(self):
         sleep(2)
